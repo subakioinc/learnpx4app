5a1983909510e9f2a60c1d5fcbfee086ad93894d
Subak.io <50165193+subakioinc@users.noreply.github.com>
2021-04-07 07:20:51 +0900

    Update mymodule_subscribe.cpp

--- mymodule_subscribe/mymodule_subscribe.cpp
+++ mymodule_subscribe/mymodule_subscribe.cpp
@@ -33,11 +33,10 @@
 
 #include "mymodule_subscribe.h"
 
-#include <px4_getopt.h>
-#include <px4_log.h>
-#include <px4_posix.h>
+#include <px4_platform_common/getopt.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/posix.h>
 
-#include <uORB/topics/parameter_update.h>
 #include <uORB/topics/mission_subak.h>
 
 
